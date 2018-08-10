a2cb62dd05fa5e0ab45b628493dc063c5ac8ce0b
timowestland <timo.westland@incentro.com>
2018-08-10 15:06:00 +0200

    Add exports of components could be required in bloomreach implementations

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ export { default as RenderCmsComponent } from './cms-components/core/render-cms-
 
 // exports for building custom components
 export { default as ContentComponentWrapper } from './cms-components/core/content-component-wrapper';
+export { default as ManageContentButton } from './cms-components/core/manage-content-button'
 export { default as Placeholder } from './cms-components/core/placeholder';
 export { default as createLink } from './utils/create-link';
 export { default as parseDate } from './utils/date';
 export { default as getNestedObject } from './utils/get-nested-object';
+export { default as getConfigurationForPath } from './utils/get-configuration-for-path'
 export { default as getImageUrl } from './utils/image-url';
 export { default as parseAndRewriteLinks } from './utils/link-rewriter';
