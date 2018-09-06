0243f6d036592251fabaf65d3ddb225542b05284
Robbert Kauffman <robbert.kauffman@bloomreach.com>
2018-09-06 11:02:57 -0400

    added support for preview detection using query string

--- src/utils/parse-request.js
+++ src/utils/parse-request.js
@@ -12,9 +12,16 @@ export default function parseRequest(request = {}, cmsUrls) {
     hostname = hostname.substring(0, hostname.indexOf(':'));
   }
 
+  // detect if in CMS/preview mode
+  let preview = hasPreviewQueryParameter(urlPath);
+  if (!preview) {
+    preview = isMatchingPreviewHostname(hostname, cmsUrls);
+  }
+  
   // TODO: error handling
   const results = cmsUrls.regexp.exec(urlPath);
 
+  let path = '';
   if (results) {
     // find the index of preview and path in keys, so we can look up the corresponding results in the results array
     const pathIdx = cmsUrls.regexpKeys.findIndex(function (obj) {
@@ -24,27 +31,37 @@ export default function parseRequest(request = {}, cmsUrls) {
       return obj.name === 'previewPrefix';
     });
 
-    const path = results[pathIdx + 1] !== undefined ? results[pathIdx + 1] : '';
-
-    // if hostname is different for preview and live, 
-    // then hostname can be used to detect if we're in preview mode
-    let preview;
-    if (cmsUrls.live.hostname !== cmsUrls.preview.hostname) {
-      if (hostname === cmsUrls.live.hostname) {
-        preview = false;
-      } else if (hostname === cmsUrls.preview.hostname) {
-        preview = true;
-      } else {
-        preview = false;
-        console.log(`Warning! Could not detect preview mode for ${hostname}. Check if cmsUrls have been properly set. Setting preview to false.`);
-      }
-    } else {
+    path = results[pathIdx + 1] !== undefined ? results[pathIdx + 1] : '';
+    
+    if (!preview) {
       // otherwise use preview-prefix in URL-path to detect preview mode
       preview = results[previewIdx + 1] !== undefined ? true : false;
     }
-
-    return { path: path, preview: preview };
   }
-  // TODO: error handling
-  return { path: '', preview: false };
+
+  return { path: path, preview: preview };
+}
+
+function hasPreviewQueryParameter(urlPath) {
+  const queryStringIdx = urlPath.indexOf('?');
+  if (queryStringIdx !== -1) {
+    const queryString = urlPath.substring(queryStringIdx);
+    if (queryString.indexOf('?bloomreach-preview=true') !== -1 || queryString.indexOf('&bloomreach-preview=true') !== -1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// if hostname is different for preview and live, 
+// then hostname can be used to detect if we're in preview mode
+function isMatchingPreviewHostname(hostname, cmsUrls) {
+  if (cmsUrls.live.hostname !== cmsUrls.preview.hostname) {
+    if (hostname === cmsUrls.preview.hostname) {
+      return true;
+    } else if (hostname !== cmsUrls.live.hostname) {
+      console.log(`Warning! Could not detect preview mode for ${hostname}. Check if cmsUrls have been properly set. Setting preview to false.`);
+    }
+  }
+  return false;
 }
