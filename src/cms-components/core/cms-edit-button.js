8f3af4f225f639c3c12568537885f449b47be734
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2019-08-01 09:24:27 +0200

    CMS-12083 Fix the content management button reference on a component update

--- src/cms-components/core/cms-edit-button.js
+++ src/cms-components/core/cms-edit-button.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import { addBeginComment } from '../../utils/add-html-comment';
+import getNestedObject from '../../utils/get-nested-object';
 
 export default class CmsEditButton extends React.Component {
-  addButton(htmlElm, configuration, preview) {
+  constructor(props) {
+    super(props);
+
+    this.placeholder = React.createRef();
+  }
+
+  addButton(htmlElm) {
+    const { configuration, preview } = this.props;
     addBeginComment(htmlElm, 'afterbegin', configuration, preview);
   }
 
-  render() {
-    const { configuration, preview } = this.props;
+  shouldComponentUpdate(nextProps) {
+    const path = ['_meta', 'beginNodeSpan', 0, 'data'];
 
-    if (!preview) {
+    return getNestedObject(this.props.configuration, path) !== getNestedObject(nextProps.configuration, path);
+  }
+
+  componentDidMount() {
+    if (!this.placeholder.current) {
+      return;
+    }
+
+    this.addButton(this.placeholder.current);
+  }
+
+  componentDidUpdate() {
+    const placeholder = this.placeholder.current;
+    if (!placeholder) {
+      return;
+    }
+
+    Array.from(placeholder.childNodes)
+      .forEach(node => node.remove());
+    placeholder.removeAttribute('class');
+    this.addButton(placeholder);
+  }
+
+  render() {
+    if (!this.props.preview) {
       return null;
     }
 
     return (
       <div style={{ position: 'relative' }}>
-        <span ref={(editContentElm) => { this.addButton(editContentElm, configuration, preview); }}/>
+        <span ref={this.placeholder}/>
       </div>
     );
   }
